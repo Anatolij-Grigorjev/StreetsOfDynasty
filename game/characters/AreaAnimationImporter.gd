658f69cb0e9461b7251c2e823e901f330b7f8a33
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2020-01-30 19:59:09 +0200

    add more track generation code, switch to godot 3.2

--- game/characters/AreaAnimationImporter.gd
+++ game/characters/AreaAnimationImporter.gd
@@ -11,7 +11,7 @@ export(NodePath) var area_group_path: NodePath
 
 
 onready var animator: AnimationPlayer = get_node(animator_path)
-onready var area_group = get_node(area_group_path)
+onready var area_group_name: String = get_node(area_group_path).name
 
 
 func _ready():
@@ -23,15 +23,54 @@ func _ready():
 	if (not area_timing_json):
 		return
 	
-	animator
+	for timing_track in area_timing_json:
+		_build_timing_track(timing_track)
 	
 
-
+func _build_timing_track(timing_track_json: Dictionary) -> void:
+	var animation: Animation = \
+		animator.get_animation(timing_track_json.animation)
+	if (not is_instance_valid(animation)):
+		return
+	var key_time: float = _get_key_time(timing_track_json.timing)
+	if (key_time < 0.0):
+		key_time = animation.length
+	var animation_key: Dictionary = \
+		_build_switch_area_key(timing_track_json.enable_area)
+	var track_idx = animation.add_track(Animation.TYPE_METHOD)
+	animation.track_set_path(track_idx, area_group_name)
+	animation.track_insert_key(track_idx, key_time, animation_key)
+	
+	
+	
+func _build_switch_area_key(switch_area: String) -> Dictionary:
+	return {
+		'name': "switch_to_area",
+		'arg_count': 1,
+		'args': [
+			{
+				'type': 'String',
+				'value': switch_area
+			}
+		]
+	}
+	
+	
+	
+func _get_key_time(json_timing: String) -> float:
+	if (not json_timing or json_timing == "start"):
+		return 0.0
+	elif (json_timing == "end"):
+		return -1.0
+	else:
+		return json_timing.to_float()
+	
+	
 
 func _are_parameters_valid() -> bool:
 	return (
 		is_instance_valid(animator) 
-		and is_instance_valid(area_group)
+		and area_group_name
 		and animation_data_path
 	)
 	
@@ -49,7 +88,7 @@ func _get_valid_file(path: String) -> File:
 func _get_valid_json(text: String):
 	var json_errors := validate_json(text)
 	if (json_errors):
-		print("Error parsing AreaGroup animation JSON %s, got error: '%s'" % [file_text, json_errors])
+		print("Error parsing AreaGroup animation JSON %s, got error: '%s'" % [text, json_errors])
 		return null
 		
 	return parse_json(text)
