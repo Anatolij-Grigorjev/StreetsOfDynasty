5fa84d4f34f7b4f71971701f47969fe182d6c324
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2020-02-12 19:23:10 +0200

    add spark, try position

---
+++ game/Utils.gd
@@ -0,0 +1,10 @@
+class_name Utils
+"""
+Static utility methods
+"""
+
+static func rand_point(range_x: float, range_y: float) -> Vector2:
+	return Vector2(
+		rand_range(-range_x, range_x),
+		rand_range(-range_y, range_y)
+	)
--- game/characters/Yu/YuCharacter.gd
+++ game/characters/Yu/YuCharacter.gd
@@ -1,6 +1,7 @@
 extends KinematicBody2D
 class_name CharacterTemplate
 
+var SparkBlunt = preload("res://characters/spritefx/SparkBlunt.tscn")
 
 export(Vector2) var move_speed: Vector2 = Vector2(4 * 64, 2 * 64)
 var velocity = Vector2()
@@ -23,9 +24,19 @@ func _process(delta: float) -> void:
 	
 func _on_hitbox_hit(hitbox: Hitbox, attackbox: AttackBox):
 	is_hurting = true
+	var hitbox_center_position = (
+			hitbox.shape.position 
+			+ hitbox.shape.shape.extents
+		)
 	#TODO: instance correct damage spark
 	match(attackbox.damage_type):
 		AttackBox.DamageType.BLUNT:
+			var spark = SparkBlunt.instance()
+			var position = Utils.rand_point(5, 5) + hitbox_center_position
+			var rotation = randf() * 360
+			get_tree().get_root().add_child(spark)
+			spark.global_position = position
+			spark.global_rotation_degrees = rotation
 			pass
 		AttackBox.DamageType.BLEEDING:
 			pass
