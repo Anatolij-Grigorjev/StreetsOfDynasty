65f6123073a75842d3aa4f553ecfb85713c3f3b3
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2020-02-22 10:55:51 +0200

    add A2 kick animation

--- game/characters/Yu/AttackA1.gd
+++ game/characters/Yu/AttackA1.gd
@@ -25,6 +25,7 @@ func process_state(delta: float):
 	
 func enter_state(prev_state: String):
 	.enter_state(prev_state)
+	_move_with_attack(25, 0.25)
 	entity.anim.play("attack_a1")
 	fsm.hitboxes.switch_to_area("AttackA1")
 	#disable attack areas before chosing one to enable
@@ -33,3 +34,18 @@ func enter_state(prev_state: String):
 	
 func exit_state(next_state: String):
 	.exit_state(next_state)
+	
+	
+func _move_with_attack(move_distance: float, move_duration: float):
+	if ($AttackMove.is_active()):
+		return
+	var move_to = entity.global_position.x + entity.scale.x * move_distance
+	$AttackMove.interpolate_property(
+		entity, 'global_position:x', 
+		null, move_to, move_duration, 
+		Tween.TRANS_EXPO, Tween.EASE_OUT
+	)
+	entity.LOG.info("move {} -> {} over {}s", 
+		[entity.global_position.x, move_to, move_duration]
+	)
+	$AttackMove.start()
