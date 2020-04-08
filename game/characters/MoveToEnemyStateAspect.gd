a427ff93b1c306ef2361696e992f3de5a380feb8
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2020-04-08 13:55:15 +0300

    start setup raycast nodes

--- game/characters/MoveToEnemyStateAspect.gd
+++ game/characters/MoveToEnemyStateAspect.gd
@@ -33,6 +33,40 @@ export(int, LAYERS_2D_PHYSICS) var collision_mask := 0
 
 onready var state: PerpetualState = get_parent()
 
+var rays_array: Node2D
+
+
+func _ready():
+	call_deferred("_build_rays_arc")
+
+
+func _build_rays_arc():
+	#create container
+	rays_array = Node2D.new()
+	rays_array.name = "MoveToEnemyAspectRays"
+	#add tree to character
+	entity.add_child(rays_array)
+	
+	#add rays
+	var direct_ray = _build_raycast_to(Vector2(check_enemy_max_distance, 0))
+	rays_array.add_child(direct_ray)
+	for pos_y_increment in range(arc_ray_partition_size, check_enemy_arc_radius, arc_ray_partition_size):
+		var ray_above = _build_raycast_to(Vector2(check_enemy_max_distance, pos_y_increment))
+		rays_array.add_child(ray_above)
+	for pos_y_increment in range(-arc_ray_partition_size, -check_enemy_arc_radius, -arc_ray_partition_size):
+		var ray_below = _build_raycast_to(Vector2(check_enemy_max_distance, pos_y_increment))
+		rays_array.add_child(ray_below)
+
+
+func _build_raycast_to(destination: Vector2) -> RayCast2D:
+	var ray = RayCast2D.new()
+	ray.collide_with_areas = true
+	ray.collide_with_bodies = false
+	ray.cast_to = destination
+	ray.collision_mask = collision_mask
+	
+	return ray
+
 
 func enter_state(prev_state: String):
 	.enter_state(prev_state)
