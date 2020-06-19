7794c0b1a1bbbeb1cfd39e0c1a70a934809f53a4
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2020-06-19 20:26:39 +0300

    add sprite flash super sound

--- game/characters/SpriteColorFlashRequestor.gd
+++ game/characters/SpriteColorFlashRequestor.gd
@@ -6,20 +6,28 @@ request a color flash happen with given parameters
 """
 signal color_flash_requested(color, duration, blend, buildup_slice)
 
+export(AudioStream) var flash_sound
 export(Color) var color := Color.blue
 export(float) var total_duration := 0.3
 export(float) var max_blend := 0.8
 export(float) var buildup_slice := 0.7
 
 
+onready var audio_player: AudioStreamPlayer = _create_audio_player()
+
 func _ready():
 	add_to_group("color_flashers", true)
 	set_process(false)
 	set_physics_process(false)
+	audio_player.stream = flash_sound
+	audio_player.autoplay = false
+	audio_player.pitch_scale = 2.5
 	
 	
 func request_color_flash(color = Color.blue, total_duration = 0.3, max_blend = 0.8, buildup_slice = 0.7):
+	audio_player.play(0)
 	emit_signal("color_flash_requested", color, total_duration, max_blend, buildup_slice)
+	
 
 
 func build_color_flash(params: Dictionary = {}):
@@ -29,3 +37,9 @@ func build_color_flash(params: Dictionary = {}):
 		Utils.get_or_default(params, 'max_blend', max_blend),
 		Utils.get_or_default(params, 'buildup_slice', buildup_slice)
 	)
+
+
+func _create_audio_player() -> AudioStreamPlayer:
+	var player = AudioStreamPlayer.new()
+	add_child(player)
+	return player
