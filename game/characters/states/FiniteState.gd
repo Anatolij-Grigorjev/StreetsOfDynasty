14e768f743017619df44e7ddcfebd0e39d10a9f8
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2020-08-01 10:34:44 +0300

    change finite state to respond to animation length

--- game/characters/states/FiniteState.gd
+++ game/characters/states/FiniteState.gd
@@ -1,47 +1,41 @@
 extends PerpetualState
 class_name FiniteState
 """
-A type of standartized character state that lasts a fixes amount
-of time and then moves on to Idle or whichever is configured
+A type of character state that lasts the duration of its state animation
 
-Can have effective state ranges (state commit start/end)
-during which the state is locked and ignores external inputs
+Can have effective state ranges, counted against animation time
+(state commit start/end) during which the state is locked
+ and ignores external inputs
 """
 #when character starts ignoring external input to chagne state
-export(float) var state_commit_start_sec := 0.0
+export(float) var state_commit_anim_sec := 0.0
 ##when character stops ignoring external input to chagne state
-export(float) var state_commit_end_sec := 0.93
-#total length of finite state. after this amount elapses character
-#forcefully transitions to configured 'next_state' or Idle
-export(float) var state_length_sec := 1.0
-
-#current elapsed state time
-var state_time: float = 0.0
+export(float) var state_commit_end_anim_sec := 0.93
 #can the state be changed into another state?
 #this is 'true' while 'state_time' is out of commit ranges
 var can_change_state := true
 #is the state over and should transition
 var is_state_over := true
+#handle on entity animator
+var animator: AnimationPlayer
+
+
+func _init():
+	call_deferred("_inject_entity_animator")
 
 
 func process_state(delta: float):
 	.process_state(delta)
 	if (not is_state_over):
 		_check_can_change_state()
-		_check_state_finished()
-	state_time += delta
 	
 	
 func _check_can_change_state():
+	var state_time = _get_state_animation_time()
 	can_change_state = not (
-		state_time >= state_commit_start_sec 
-		and state_time <= state_commit_end_sec
+		state_time >= state_commit_anim_sec 
+		and state_time <= state_commit_end_anim_sec
 	)
-	
-	
-func _check_state_finished():
-	if (not is_state_over and state_time >= state_length_sec):
-		_finish_state()
 		
 		
 func _finish_state():
@@ -51,5 +45,21 @@ func _finish_state():
 	
 func enter_state(prev_state: String):
 	.enter_state(prev_state)
-	state_time = 0.0
 	is_state_over = false
+	
+	
+func _get_state_animation_time() -> float:
+	if (animator.current_animation == state_animation):
+		return animator.current_animation_position
+	else:
+		return 0.0
+	
+	
+func _inject_entity_animator():
+	animator = entity.anim as AnimationPlayer
+	
+	
+func _on_AnimationPlayer_animation_finished(anim_name: String):
+	._on_AnimationPlayer_animation_finished(anim_name)
+	if (anim_name == state_animation):
+		_finish_state()
